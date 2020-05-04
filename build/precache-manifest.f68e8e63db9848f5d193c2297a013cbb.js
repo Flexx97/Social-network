913750e1d737ae9461ff300cd9a260f8a03e13b6
Flexx97 <47747293+Flexx97@users.noreply.github.com>
2020-05-04 15:20:58 +0600

    First commit

---
+++ build/precache-manifest.f68e8e63db9848f5d193c2297a013cbb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3fbf39d45fc167ec3a8f7765306fb65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e2b997ca7b953d6320c",
+    "url": "/static/css/main.a1542275.chunk.css"
+  },
+  {
+    "revision": "d919e74ee21b69f33dc4",
+    "url": "/static/js/2.d9fda634.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.d9fda634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2b997ca7b953d6320c",
+    "url": "/static/js/main.54bb3a44.chunk.js"
+  },
+  {
+    "revision": "e42c12548c57595ecc76",
+    "url": "/static/js/runtime-main.9a542621.js"
+  },
+  {
+    "revision": "8f8f3d19c8bbe65f1169893f70d1f722",
+    "url": "/static/media/audio.8f8f3d19.svg"
+  },
+  {
+    "revision": "65d84bf5be0af4ed93b083ad667f752d",
+    "url": "/static/media/user.65d84bf5.png"
+  }
+]);
